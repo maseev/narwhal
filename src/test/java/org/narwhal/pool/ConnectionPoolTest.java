28433f5d1b74fadb97582b78144034e3eb6bcd49
Miron Aseev <aseev.miron@gmail.com>
2013-03-04 18:34:00 +0900

    setSizeTest unit test method has been implemented

--- src/test/java/org/narwhal/pool/ConnectionPoolTest.java
+++ src/test/java/org/narwhal/pool/ConnectionPoolTest.java
@@ -1,8 +1,12 @@
 package org.narwhal.pool;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.narwhal.core.DatabaseInformation;
+
+import java.sql.SQLException;
 
 /**
  * @author Miron Aseev
@@ -10,8 +14,32 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class ConnectionPoolTest {
 
-    @Test
-    public void setSizeTest() {
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost/bank";
+    private static String username = "lrngsql";
+    private static String password = "lrngsql";
 
+
+    @Test
+    public void setSizeTest() throws ClassNotFoundException {
+        DatabaseInformation databaseInformation = new DatabaseInformation(driver, url, username, password);
+        ConnectionPool pool;
+        int expectedPoolSize = 10;
+        int result = 0;
+
+        try {
+            pool = new ConnectionPool(databaseInformation, 1, 1);
+
+            try {
+                pool.setSize(expectedPoolSize);
+                result = pool.getSize();
+            } finally {
+                pool.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        Assert.assertEquals(expectedPoolSize, result);
     }
 }
