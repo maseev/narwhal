09968194781d803b7c9234e7161a282938a6b1fd
Miron Aseev <aseev.miron@gmail.com>
2013-05-07 14:46:24 +0900

    Fixed comments

--- src/main/java/org/narwhal/annotation/Column.java
+++ src/main/java/org/narwhal/annotation/Column.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 
 /**
- * The <code>Column</code> annotation marks particular
+ * The <code>Column</code> annotation is used to mark the particular
  * field of the class which maps on the column of the database table.
  *
  * @author Miron Aseev
@@ -17,7 +17,7 @@ import java.lang.annotation.Target;
 public @interface Column {
 
     /**
-     * Returns database column name.
+     * Returns the database column name.
      *
      * @return Database column name.
      * */
