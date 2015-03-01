0be33009a9e9d3b730bdcd7d9c35168c47f30ba3
Miron Aseev <aseev.miron@gmail.com>
2015-03-01 10:02:00 +0600

    Refactored QueryCreator by removing duplicate pieces of code as well as introduced some auxiliary methods for query creation

--- src/main/java/org/narwhal/query/QueryCreator.java
+++ src/main/java/org/narwhal/query/QueryCreator.java
@@ -1,34 +1,22 @@
 package org.narwhal.query;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * QueryCreator is a class which is responsible for creating string representation of the CRUD SQL statements.
- * This class implementation doesn't depend on any particular database syntax.
  * */
-public abstract class QueryCreator {
+public class QueryCreator {
 
     /**
      * Builds up a string representation of an INSERT SQL statement for the subsequent usage in a prepared statement.
      *
      * @param tableName String representation of the table name that maps to the particular entity.
      * @param columns Array of the table's columns
-     * @param primaryColumnName String representation of the primary column.
      * @return String representation of the INSERT SQL statement.
      * */
-    public String makeInsertQuery(String tableName, String[] columns, String primaryColumnName) {
-        StringBuilder builder = new StringBuilder("INSERT INTO ");
-        builder.append(tableName);
-        builder.append(" VALUES (");
-
-        for (int i = 0; i < columns.length; ++i) {
-            if (i > 0) {
-                builder.append(',');
-            }
-
-            builder.append('?');
-        }
-        builder.append(')');
-
-        return builder.toString();
+    public String buildInsertQuery(String tableName, List<String> columns) {
+        return "INSERT INTO " + tableName + " (" + join(columns, ",") + ") VALUES (" + join("?", columns.size(), ",") + ')';
     }
 
     /**
@@ -36,44 +24,22 @@ public abstract class QueryCreator {
      *
      * @param tableName String representation of the table name that maps to the particular entity.
      * @param columns Array of the table's columns
-     * @param primaryColumnName String representation of the primary column.
+     * @param primaryKeys Primary column names.
      * @return String representation of the SELECT SQL statement.
      * */
-    public String makeSelectQuery(String tableName, String[] columns, String primaryColumnName) {
-        StringBuilder builder = new StringBuilder("SELECT ");
-
-        for (int i = 0; i < columns.length; ++i) {
-            if (i > 0) {
-                builder.append(',');
-            }
-
-            builder.append(columns[i]);
-        }
-
-        builder.append(" FROM ");
-        builder.append(tableName);
-        builder.append(" WHERE ");
-        builder.append(primaryColumnName);
-        builder.append(" = ?");
-
-        return builder.toString();
+    public String buildSelectQuery(String tableName, List<String> columns, List<String> primaryKeys) {
+        return "SELECT " + join(columns, ",") + " FROM " + tableName + buildWhereClause(primaryKeys);
     }
 
     /**
      * Builds up a string representation of an DELETE SQL statement for the subsequent usage in a prepared statement.
      *
      * @param tableName String representation of the table name that maps to the particular entity.
-     * @param primaryColumnName String representation of the primary column.
+     * @param primaryKeys Primary column names.
      * @return String representation of the DELETE SQL statement.
      * */
-    public String makeDeleteQuery(String tableName, String primaryColumnName) {
-        StringBuilder builder = new StringBuilder("DELETE FROM ");
-        builder.append(tableName);
-        builder.append(" WHERE ");
-        builder.append(primaryColumnName);
-        builder.append(" = ?");
-
-        return builder.toString();
+    public String buildDeleteQuery(String tableName, List<String> primaryKeys) {
+        return "DELETE FROM " + tableName + buildWhereClause(primaryKeys);
     }
 
     /**
@@ -81,25 +47,36 @@ public abstract class QueryCreator {
      *
      * @param tableName String representation of the table name that maps to the particular entity.
      * @param columns Array of the table's columns
-     * @param primaryColumnName String representation of the primary column.
+     * @param primaryKeys Primary column names.
      * @return String representation of the UPDATE SQL statement.
      * */
-    public String makeUpdateQuery(String tableName, String[] columns, String primaryColumnName) {
-        StringBuilder builder = new StringBuilder("UPDATE ");
-        builder.append(tableName);
-        builder.append(" SET ");
+    public String buildUpdateQuery(String tableName, List<String> columns, List<String> primaryKeys) {
+        return "UPDATE " + tableName + " SET " + join(columns, ",", " = ?") + buildWhereClause(primaryKeys);
+    }
 
-        for (int i = 0; i < columns.length; ++i) {
+    private String buildWhereClause(List<String> primaryKeys) {
+        return " WHERE " + join(primaryKeys, " AND ", " = ?");
+    }
+
+    private String join(String filler, int size, String delimiter) {
+        return join(Collections.nCopies(size, filler), delimiter);
+    }
+
+    private String join(List<String> values, String delimiter) {
+        return join(values, delimiter, "");
+    }
+
+    private String join(List<String> values, String delimiter, String afterValue) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < values.size(); i++) {
             if (i > 0) {
-                builder.append(',');
+                builder.append(delimiter);
             }
 
-            builder.append(columns[i]);
-            builder.append(" = ?");
+            builder.append(values.get(i));
+            builder.append(afterValue);
         }
-        builder.append(" WHERE ");
-        builder.append(primaryColumnName);
-        builder.append(" = ?");
 
         return builder.toString();
     }
