3119dcfa9ba363fbebaeff2ddda92c784bc7703b
Miron Aseev <aseev.miron@gmail.com>
2015-03-01 12:37:05 +0600

    Fixed ConnectionPoolTest, sow now we are using h2 in memory database instead of standalone postgresql instance

--- src/test/java/org/narwhal/pool/ConnectionPoolTest.java
+++ src/test/java/org/narwhal/pool/ConnectionPoolTest.java
@@ -2,25 +2,17 @@ package org.narwhal.pool;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.narwhal.core.ConnectionInformation;
 import org.narwhal.core.DatabaseConnection;
-import org.narwhal.pool.ConnectionPool;
 
-/**
- * @author Miron Aseev
- */
-@RunWith(JUnit4.class)
 public class ConnectionPoolTest {
-
 
     @Test
     public void setSizeTest() {
-        final String driver   = "org.postgresql.Driver";
-        final String url      = "jdbc:postgresql://localhost/test";
-        final String username = "postgres";
-        final String password = "admin";
+        final String driver = "org.h2.Driver";
+        final String url = "jdbc:h2:mem:test";
+        final String username = "user";
+        final String password = "password";
         ConnectionInformation connectionInformation = new ConnectionInformation(driver, url, username, password);
         ConnectionPool pool;
         int expectedPoolSize = 10;
